9da365cec9cfe565e8b8dbff9be9eeece1970bb3
dain0826 <62243967+dain0826@users.noreply.github.com>
2022-04-14 01:55:35 +0900

    최댓값 구하기

---
+++ src/practice/chap01/Max4.java
@@ -0,0 +1,23 @@
+package practice.chap01;
+
+public class Max4 {
+    static int max4(int a, int b, int c, int d){
+        int max = a;
+        if(b>max) max = b;
+        if(c>max) max = c;
+        if(d>max) max = d;
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("max(1,2,3,4) = "+max4(1,2,3,4));
+        System.out.println("max(1,2,4,3) = "+max4(1,2,4,3));
+        System.out.println("max(1,3,2,4) = "+max4(1,3,2,4));
+        System.out.println("max(4,2,1,4) = "+max4(4,2,1,4));
+        System.out.println("max(3,1,4,2) = "+max4(3,1,4,2));
+
+
+
+    }
+}
