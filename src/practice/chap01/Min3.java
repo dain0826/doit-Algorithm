f326f56d5fa9dc6237d8feacf501f697934455f8
dain0826 <62243967+dain0826@users.noreply.github.com>
2022-04-14 01:55:54 +0900

    최솟값 구하기

---
+++ src/practice/chap01/Min3.java
@@ -0,0 +1,17 @@
+package practice.chap01;
+
+public class Min3 {
+    static int min3(int a, int b, int c){
+        int min = a;
+        if(b<min) min = b;
+        if(c<min) min = c;
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("min(1,2,3) = " + min3(1,2,3));
+        System.out.println("min(3,2,1) = " + min3(3,2,1));
+        System.out.println("min(2,3,1) = " + min3(2,3,1));
+    }
+}
---
+++ src/practice/chap01/Min4.java
@@ -0,0 +1,22 @@
+package practice.chap01;
+
+public class Min4 {
+    static int min4(int a, int b, int c, int d){
+        int min = a;
+        if(b < min) min = b;
+        if(c < min) min = c;
+        if(d < min) min = d;
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("min(1,2,3,4) = " + min4(1,2,3,4));
+        System.out.println("min(2,3,1,4) = " + min4(2,3,1,4));
+        System.out.println("min(3,4,2,1) = " + min4(3,4,2,1));
+        System.out.println("min(1,4,2,3) = " + min4(1,4,2,3));
+        System.out.println("min(2,1,4,3) = " + min4(2,1,4,3));
+        System.out.println("min(3,2,4,1) = " + min4(3,2,4,1));
+
+    }
+}
